3fb603628e463d7450c4436f54abfce48bf594ce
Moisés Alves <moisesalves1@gmail.com>
2023-09-28 14:32:53 -0300

    ValueObject de Email + InvalidEmailException

--- JwtStore.Core/AccountContext/ValueObjects/Email.cs
+++ JwtStore.Core/AccountContext/ValueObjects/Email.cs
@@ -1,13 +1,35 @@
 ﻿using JwtStore.Core.SharedContext.ValueObjects;
 using JwtStore.Core.SharedContext.Extensions;
+using System.Text.RegularExpressions;
+using JwtStore.Core.AccountContext.ValueObjects.Exceptions;
 
 namespace JwtStore.Core.AccountContext.ValueObjects
 {
-    public class Email : ValueObject
+    public partial class Email : ValueObject
     {
-        private const string Pattern = @"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$";
+
+        public Email(string address)
+        {
+            InvalidEmailException.ThrowIfNull(address);
+
+            Address = address.Trim().ToLower();
+
+            InvalidEmailException.ThrowIfInvalid(address);
+
+        }
 
         public string Address { get; }
-        public string Hash => Address.ToBase64()
+        public string Hash => Address.ToBase64();
+
+        public static implicit operator string(Email email)
+            => email.ToString();
+
+        public static implicit operator Email(string address)
+            => new Email(address);
+
+        public override string ToString()
+            => Address;
+
+        
     }
 }
---
+++ JwtStore.Core/AccountContext/ValueObjects/Exceptions/InvalidEmailException.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace JwtStore.Core.AccountContext.ValueObjects.Exceptions
+{
+    public partial class InvalidEmailException : Exception
+    {
+        private const string Pattern = @"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$";
+        private const string DefaultErrorMessage = "E-mail inválido";
+        public InvalidEmailException(string message = DefaultErrorMessage) 
+            : base(message) 
+        {
+            
+        }
+
+        public static void ThrowIfInvalid(string address, string message = DefaultErrorMessage)
+        {
+            if (string.IsNullOrEmpty(address))
+                throw new InvalidEmailException(message);
+            if (!EmailRegex().IsMatch(address))
+                throw new InvalidEmailException();
+        }
+
+        public static void ThrowIfNull(string? item, string message = DefaultErrorMessage)
+        {
+            if (string.IsNullOrEmpty(item))
+                throw new InvalidEmailException(message);
+
+        }
+
+        [GeneratedRegex(Pattern)]
+        private static partial Regex EmailRegex();
+    }
+}
