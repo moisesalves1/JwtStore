fb0ae20cc92ad46954297580d12cde7f69bda13a
Moisés Alves <moisesalves1@gmail.com>
2023-09-28 14:51:59 -0300

    ValueObject de Verificação de E-mail e InvalidVerificationException

---
+++ JwtStore.Core/AccountContext/ValueObjects/Exceptions/InvalidVerificationException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace JwtStore.Core.AccountContext.ValueObjects.Exceptions
+{
+    public class InvalidVerificationException : Exception
+    {
+        private const string DefaultErrorMessage = "Código de verificação inválido";
+        public InvalidVerificationException(string message = DefaultErrorMessage) 
+            : base(message) 
+        {
+            
+        }
+
+        public static void ThrowIfInvalid(string code, string Code, DateTime? ExpiresAt, bool IsActive, string message = DefaultErrorMessage)
+        {
+            if (IsActive)
+                throw new InvalidVerificationException("Este item já foi ativado");
+            if (ExpiresAt < DateTime.UtcNow)
+                throw new InvalidVerificationException("Este código já expirou");
+            if (!string.Equals(code.Trim(), Code.Trim(), StringComparison.CurrentCultureIgnoreCase))
+                throw new InvalidVerificationException();
+        }
+
+    }
+}
---
+++ JwtStore.Core/AccountContext/ValueObjects/Verification.cs
@@ -0,0 +1,28 @@
+﻿using JwtStore.Core.AccountContext.ValueObjects.Exceptions;
+using JwtStore.Core.SharedContext.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JwtStore.Core.AccountContext.ValueObjects
+{
+    public class Verification : ValueObject
+    {
+        public string Code { get; } = Guid.NewGuid().ToString("N")[..6].ToUpper();
+        public DateTime? ExpiresAt { get; private set; } = DateTime.UtcNow.AddMinutes(5);
+        public DateTime? VerifiedAt { get; private set; } = null;
+        public bool IsActive => VerifiedAt != null && ExpiresAt == null;
+
+        public void Verify(string code)
+        {
+            InvalidVerificationException.ThrowIfInvalid(code, Code, ExpiresAt, IsActive);
+
+            ExpiresAt = null;
+            VerifiedAt = DateTime.UtcNow;
+        }
+
+    }
+}
