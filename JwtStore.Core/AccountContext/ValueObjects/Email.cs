57d44181a9649abe526554f28684e5c3220ad8ee
Moisés Alves <moisesalves1@gmail.com>
2023-09-28 14:17:40 -0300

    String Extension

---
+++ JwtStore.Core/AccountContext/ValueObjects/Email.cs
@@ -0,0 +1,13 @@
+﻿using JwtStore.Core.SharedContext.ValueObjects;
+using JwtStore.Core.SharedContext.Extensions;
+
+namespace JwtStore.Core.AccountContext.ValueObjects
+{
+    public class Email : ValueObject
+    {
+        private const string Pattern = @"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$";
+
+        public string Address { get; }
+        public string Hash => Address.ToBase64()
+    }
+}
