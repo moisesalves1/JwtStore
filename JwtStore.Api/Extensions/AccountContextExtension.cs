19ef5790e6cb11ff6b11fc47f100f91c60f8280e
Moises Alves <moisesalves1@gmail.com>
2023-10-05 17:17:22 -0300

    Configurando a API

---
+++ JwtStore.Api/Extensions/AccountContextExtension.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace JwtStore.Api.Extensions
+{
+    public static class AccountContextExtension
+    {
+        public static void AddAccountContext(this WebApplicationBuilder builder)
+        {
+
+        }
+
+        public static void MapAccountEndpoints(this WebApplication app)
+        {
+
+        }
+    }
+}
