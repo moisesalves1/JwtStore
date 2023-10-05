2bfd5f389cb3d8c71a022dec5bb5aba464db724d
Moises Alves <moisesalves1@gmail.com>
2023-10-05 13:22:16 -0300

    Repositorios e Serviços

--- JwtStore.Core/Contexts/AccountContext/UseCases/Create/Handler.cs
+++ JwtStore.Core/Contexts/AccountContext/UseCases/Create/Handler.cs
@@ -1,12 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using JwtStore.Core.Contexts.AccountContext.UseCases.Create.Contracts;
 
 namespace JwtStore.Core.Contexts.AccountContext.UseCases.Create
 {
     public class Handler
     {
+        private readonly IRepository _repository;
+        private readonly IService _service;
+
+        public Handler(IRepository repository, IService service)
+        {
+            _repository = repository;
+            _service = service;
+        }
+
+        public async Task<Response> Handle(Request request, CancellationToken cancellationToken)
+        {
+
+        }
     }
 }
